dbfa185cd0e3864b1761fe468bee523ecd7c82ad
Tara Vancil <tbvanc@gmail.com>
2017-07-22 12:21:14 -0500

    Properly render the image based on orientation metadata in EXIF

--- js/album.js
+++ js/album.js
@@ -5,6 +5,13 @@
     return
   }
 
+  const IMAGE_ROTATION = {
+    1: 'rotate(0deg)',
+    3: 'rotate(180deg)',
+    6: 'rotate(90deg)',
+    8: 'rotate(270deg)'
+  };
+
   // setup
   let archive, archiveInfo, albums
   let selectedImages = []
@@ -98,6 +105,10 @@
 
           reader.onload = async function () {
             const path = `/images/${file.name}`
+            const orientation = readOrientationMetadata(reader.result)
+
+            // write the orientiation metadata to localStorage
+            localStorage.setItem(`${archive.url}${path}`, orientation)
 
             // only write the file if it doesn't already exist
             try {
@@ -105,7 +116,7 @@
             } catch (e) {
               await archive.writeFile(path, reader.result)
               await archive.commit()
-              appendImage(path)
+              appendImage(path, orientation)
             }
           }
 
@@ -151,7 +162,9 @@
 
       // TODO sort by ctime or mtime
       for (let i = 0; i < paths.length; i++) {
-        appendImage(`/images/${paths[i]}`)
+        const path = `/images/${paths[i]}`
+        const orientation = localStorage.getItem(`${archive.url}${path}`)
+        appendImage(path, orientation)
       }
     } catch (err) {
       updatePrompt('<p>Something went wrong</p>')
@@ -159,24 +172,16 @@
     }
   }
 
-  function appendImage(src) {
-    if (typeof src !== 'string') return
-
-    const img = document.createElement('img')
-    img.src = src
-    img.addEventListener('click', onToggleSelected)
-    document.querySelector('.album-images').appendChild(img)
-  }
-
   function renderUAPrompt () {
     updatePrompt('<p>Sorry >.< This app only works in the Beaker Browser.</p><a class="btn primary" href="https://beakerbrowser.com/docs/install/">Install Beaker</a>')
   }
 
-  function appendImage(src) {
+  function appendImage(src, orientation=1) {
     if (typeof src !== 'string') return
 
     const img = document.createElement('img')
     img.src = src
+    img.style.transform = IMAGE_ROTATION[orientation]
     img.addEventListener('click', onToggleSelected)
     document.querySelector('.album-images').appendChild(img)
   }
@@ -187,8 +192,43 @@
     if (typeof html !== 'string') return
     if (html.length) {
       document.querySelector('#prompt').innerHTML = `<div class="content">${html}</div>`
+
     } else {
       document.querySelector('#prompt').innerHTML = html
     }
   }
+
+  function readOrientationMetadata (buf) {
+    const scanner = new DataView(buf)
+    let idx = 0
+    let value = 1 // Non-rotated is the default
+
+    if(buf.length < 2 || scanner.getUint16(idx) != 0xFFD8) {
+      // not a JPEG
+      return
+    }
+
+    idx += 2
+
+    let maxBytes = scanner.byteLength;
+    while(idx < maxBytes - 2) {
+      let uint16 = scanner.getUint16(idx);
+      idx += 2
+      switch(uint16) {
+        case 0xFFE1: // Start of EXIF
+          var exifLength = scanner.getUint16(idx)
+          maxBytes = exifLength - idx
+          idx += 2
+          break
+        case 0x0112: // Orientation tag
+          // Read the value, its 6 bytes further out
+          // See page 102 at the following URL
+          // http://www.kodak.com/global/plugins/acrobat/en/service/digCam/exifStandard2.pdf
+          value = scanner.getUint16(idx + 6, false)
+          maxBytes = 0; // Stop scanning
+          break
+      }
+    }
+    return value
+}
 })()
